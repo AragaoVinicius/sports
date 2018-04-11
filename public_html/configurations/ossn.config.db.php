d8d8dd4e6b870264468b22dc95ebf785a1cc2367
AragaoVinicius <36004058+AragaoVinicius@users.noreply.github.com>
2018-04-11 16:40:15 +0100

    initial

---
+++ public_html/configurations/ossn.config.db.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Open Source Social Network
+ *
+ * @package   (softlab24.com).ossn
+ * @author    OSSN Core Team <info@softlab24.com>
+ * @copyright 2014-2017 SOFTLAB24 LIMITED
+ * @license   Open Source Social Network License (OSSN LICENSE)  http://www.opensource-socialnetwork.org/licence
+ * @link      https://www.opensource-socialnetwork.org/
+ */
+
+
+// replace localhost with your database host name;
+$Ossn->host = 'localhost';
+
+// replace 3306 with your database host name;
+$Ossn->port = '3306';
+
+// replace id5368494_sportslovers with your database username;
+$Ossn->user = 'id5368494_sportslovers';
+
+// replace dublin2018 with your database password;
+$Ossn->password = 'dublin2018';
+
+// replace id5368494_sp with your database name;
+$Ossn->database = 'id5368494_sp';
