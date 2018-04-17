61eb0d52c6e481e39f5919553461de682580a9fa
AragaoVinicius <36004058+AragaoVinicius@users.noreply.github.com>
2018-04-17 16:39:47 +0100

    Update ossn.config.db.php

--- public_html/configurations/ossn.config.db.php
+++ public_html/configurations/ossn.config.db.php
@@ -8,7 +8,7 @@
  * @license   Open Source Social Network License (OSSN LICENSE)  http://www.opensource-socialnetwork.org/licence
  * @link      https://www.opensource-socialnetwork.org/
  */
-
+error_log("Connecting");
 
 // replace localhost with your database host name;
 $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
@@ -18,18 +18,29 @@ $username = $url["user"];
 $password = $url["pass"];
 $db = substr($url["path"], 1);
 
-//$conn = new mysqli($server, $username, $password, $db);
+//
 
-$Ossn->host = $server;
+$Ossn->host = 'eu-cdbr-west-02.cleardb.net';
 
 // replace 3306 with your database host name;
 $Ossn->port = '3306';
 
 // replace id5368494_sportslovers with your database username;
-$Ossn->user = $username;
+$Ossn->user = 'bd0f176f121976';
 
 // replace dublin2018 with your database password;
-$Ossn->password = $password;
+$Ossn->password = '7ae06067';
 
 // replace id5368494_sp with your database name;
-$Ossn->database = $db;
+$Ossn->database = 'heroku_7b0d115e3fe3586';
+
+$conn = new mysqli($server, $username, $password, $db);
+if(!$conn->ping()){
+  error_log("Error");
+}
+else{
+    error_log("Connected");
+}
+$conn->close();
+error_log("Closed");
+
