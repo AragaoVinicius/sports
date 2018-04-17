b605004c2d7b7677a00ff36a765efd2c386620c8
AragaoVinicius <36004058+AragaoVinicius@users.noreply.github.com>
2018-04-17 10:43:31 +0100

    Update start.php

--- public_html/system/start.php
+++ public_html/system/start.php
@@ -22,13 +22,13 @@ if (!is_file(ossn_route()->configs . 'ossn.config.site.php') && !is_file(ossn_ro
 	exit;
 }
 include_once(ossn_route()->configs . 'libraries.php');
-
+/**
 
 include_once(ossn_route()->configs . 'classes.php');
 
 include_once(ossn_route()->configs . 'ossn.config.site.php');
 include_once(ossn_route()->configs . 'ossn.config.db.php');
-/**
+
 foreach ($Ossn->classes as $class) {
     if (!include_once(ossn_route()->classes . "Ossn{$class}.php")) {
         throw new exception('Cannot include all classes');
